daf9c7d7e2bd65fad87c9022089d3943ddfa555a
jkchoi2000 <jkchoi2000@tmon.co.kr>
2019-11-13 10:48:25 +0900

    The first commit - skeleton code

---
+++ src/main/java/mine/jkchoi/springcoreexercise/SpringCoreExerciseApplication.java
@@ -0,0 +1,13 @@
+package mine.jkchoi.springcoreexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mine/jkchoi/springcoreexercise/SpringCoreExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.jkchoi.springcoreexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
